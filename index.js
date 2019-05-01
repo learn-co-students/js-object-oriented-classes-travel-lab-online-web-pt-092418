27be21118f2d8c5d060e3688a964f7b7d8835335
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-05-01 16:45:24 -0400

    Interim checkin

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function Driver  (name, Date){
+  this.name = name; 
+  Date.prototype.getDate();
+};
 
+function Route(horizonal, vertical) {
+  this.horizontal = horizontal;
+  this.vertical = vertical
+}
+Route.prototype.blocksTravelled(){
+  return horizontal - horizontal 
+  return vertical - vertical 
+};
+Route.
+  
