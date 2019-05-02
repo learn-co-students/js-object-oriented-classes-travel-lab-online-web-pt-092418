deeb47d6199c7c47b566452b0f687bac47f40046
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-05-02 09:14:49 -0400

    Interim checkin

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-function Driver  (name, Date){
+function Driver(name, dateString){
   this.name = name; 
-  Date.prototype.getDate();
+  this.startDate = new Date(dateString);
 };
 
-function Route(horizonal, vertical) {
-  this.horizontal = horizontal;
-  this.vertical = vertical
+function Route( startPoint, endPoint) {
+  this.startPoint= startPoint;
+  this.endPoint = endPoint;
 }
-Route.prototype.blocksTravelled(){
-  return horizontal - horizontal 
-  return vertical - vertical 
+
+
+Driver.prototype.yearsExperienceFromBeginningOf = function(year){
+  const yearNum = parseInt(year, 10)
+  return yearNum - this.startDate.getFullYear();
 };
-Route.
+
+const eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
+Route.prototype.blocksTravelled = function () {
+  const vertTravelled = Math.abs(parseInt(this.startPoint.vertical, 10) - parseInt(this.endPoint.vertical, 10));
+  const startIdx = eastWest.indexOf(this.startPoint.horizontal);
+  const endIdx = eastWest.indexOf(this.endPoint.horizontal);
+  const horzTravelled = Math.abs(startIdx - endIdx);
+  return vertTravelled + horzTravelled;
+}
+
+Route.prototype.estimatedTime = function(time) {
+  let d = new Date(time);
+  let n = d.getHours();
+  if (n > 8 || n > 18) {
+    let timeTraveled = blocksTravelled * 2;
+    return timeTravelled
+  }
+  else 
+    let timeTraveled = blocksTravelled * 3
+    return timeTraveled
+  
+};
+
   
